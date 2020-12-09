fa7cfb14dfb8414a275a6d3c92e5b548d9ab6a92
evgeny-tukh <jecat@mail.ru>
2020-12-09 12:00:45 +0300

    time parse

--- nmea/nmea.cpp
+++ nmea/nmea.cpp
@@ -6,6 +6,7 @@ namespace nmea {
     gpsPos pos;
     float cog, sog;
     time_t dateTime;
+    tm lastDate { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
     data _pos (& pos, sizeof (pos));
     data _cog (& cog, sizeof (cog));
@@ -13,9 +14,11 @@ namespace nmea {
     data _dateTime (& dateTime, sizeof (dateTime));
 
     void parse_GGA (sentence&);
+    void parse_ZDA (sentence&);
 
     bool composeLat (sentence&, int, double&);
     bool composeLon (sentence&, int, double&);
+    bool extratcUTC (sentence&, int, time_t&);
 }
 
 inline int digit2Int (char digit) {
@@ -80,13 +83,8 @@ bool nmea::sentence::parse (char *source) {
 void nmea::parse (nmea::sentence& snt) {
     if (memcmp (snt.type, "GGA", 3) == 0)
         parse_GGA (snt);
-}
-
-void nmea::parse_GGA (sentence& snt) {
-    gpsPos position;
-    bool ok = composeLat (snt, 2, position.lat) && composeLon (snt, 4, position.lon);
-
-    _pos.update (time (0), ok, & position);
+    else if (memcmp (snt.type, "ZDA", 3) == 0)
+        parse_ZDA (snt);
 }
 
 bool nmea::composeLat (sentence& snt, int start, double& value) {
@@ -115,4 +113,53 @@ bool nmea::composeLon (sentence& snt, int start, double& value) {
     if (snt.fields.at (start + 1).at (0) == 'W') value = - value;
 
     return true;
+}
+
+bool nmea::extratcUTC (sentence& snt, int field, time_t& timestamp) {
+    if (snt.fields.size () <= field) return false;
+    if (!lastDate.tm_year) return false;
+    if (snt.fields.at (field).length () < 6) return false;
+
+    tm result;
+    char *textValue = & snt.fields.at (field).front ();
+
+    result.tm_year = lastDate.tm_year;
+    result.tm_mon = lastDate.tm_mon;
+    result.tm_mday = lastDate.tm_mday;
+    result.tm_hour = twoDigits2Int (textValue);
+    result.tm_min = twoDigits2Int (textValue + 2);
+    result.tm_sec = twoDigits2Int (textValue + 4);
+
+    timestamp = mktime (& result) - *__timezone ();
+}
+
+void nmea::parse_GGA (sentence& snt) {
+    gpsPos position;
+    time_t timestamp, now = time (0);
+    bool ok = composeLat (snt, 2, position.lat) && composeLon (snt, 4, position.lon) && extratcUTC (snt, 1, timestamp);
+
+    _pos.update (now, ok, & position);
+    _dateTime.update (now, ok, & timestamp);
+}
+
+void nmea::parse_ZDA (sentence& snt) {
+    if (snt.fields.size () < 5) return;
+    if (snt.fields.at (1).length () < 6) return;
+
+    for (auto i = 2; i < 5; ++ i) {
+        if (snt.fields.at (i).length () < 2) return;
+    }
+
+    char *utc = & snt.fields.at (1).front ();
+
+    lastDate.tm_hour = twoDigits2Int (utc);
+    lastDate.tm_min = twoDigits2Int (utc + 2);
+    lastDate.tm_sec = twoDigits2Int (utc + 4);
+    lastDate.tm_year = atoi (snt.fields.at (4).c_str ()) - 1900;
+    lastDate.tm_mon = atoi (snt.fields.at (3).c_str ()) - 1;
+    lastDate.tm_mday = atoi (snt.fields.at (2).c_str ());
+
+    time_t timestamp = mktime (& lastDate) - *__timezone ();
+
+    _dateTime.update (time (0), true, & timestamp);
 }
